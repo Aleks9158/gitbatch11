edb8dc4588c17a0e0e9eb570688930a76a803d52
acca5859 <acca5859@gmail.com>
2024-05-10 20:41:22 +0300

    3d 3commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,7 @@
 package com.company;
 
 public class Main {
-    public static void main(String[] arg)    {
+    public static void main(String[] args)    {
         System.out.println("I am Batch11");
     }
 }
